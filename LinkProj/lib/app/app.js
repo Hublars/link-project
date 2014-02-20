360742655dbc59eec2023df425a820bc75530fd5
Hublars <lars.fangstam@gmail.com>
2014-02-20 08:45:41 +0100

    Changed and reorganized

---
+++ LinkProj/lib/app/app.js
@@ -0,0 +1,238 @@
+define(["linkCollection", "jquery", "json2", "underscore", "backbone"], function(lc) {
+
+	var LinkModel = Backbone.Model.extend({
+		defaults : {
+			photo : "assets/images/placeholder.png",
+			numId : 0
+		}
+	});
+
+	var Directory = Backbone.Collection.extend({
+		model : LinkModel,
+
+		setHeader : function(header) {
+			var that = this;
+
+			_.each(this.models, function(item) {
+				item.set("name", header);
+			}, this);
+		},
+
+		setCollectionNumber : function(newCollectionNumber) {
+			var that = this;
+
+			_.each(this.models, function(item) {
+				item.set("numId", newCollectionNumber);
+			}, this);
+		},
+	});
+
+	var LinkView = Backbone.View.extend({
+		tagName : "article",
+		className : "link-container",
+		template : $("#linkTemplate").html(),
+
+		initialize : function() {
+			//$("#header1").html("hej " + this.model.get("links").length);
+
+			//var but = this.model.get(".showMore");
+			//this.template.removeChild($el(".showMore"));
+			//this.model.set(".showMore", "");
+
+			//$("#header2").html("hej " + this.template.get("html"));
+
+			/*if (this.model.get("links").length < 1)
+			 this.model.unset()
+			 else
+			 $("#header2").html("hoj ");*/
+		},
+
+		render : function() {
+			var tmpl = _.template(this.template);
+
+			$(this.el).html(tmpl(this.model.toJSON()));
+			return this;
+		},
+
+		events : {
+			"click button.showMore" : "showMore"
+		},
+
+		showMore : function(e) {
+
+			var btnTxt = $(e.target).html();
+			var nm = this.model.get("name");
+
+			if (btnTxt === "More") {
+
+				var nextCollectionNumber = this.model.get("numId") + 1;
+
+				if (allCollections[nextCollectionNumber]) {
+
+					var existingCollectionView = allCollections[nextCollectionNumber];
+
+					var linksNew = new Directory(this.model.get("links"));
+					linksNew.setHeader(nm);
+					linksNew.setCollectionNumber(nextCollectionNumber);
+
+					existingCollectionView.collection.add(linksNew.toJSON(), {
+						silent : true
+					});
+
+					existingCollectionView.render();
+
+				} else {
+					var linksNew = new Directory(this.model.get("links"));
+					linksNew.setHeader(nm);
+
+					var subLinks = new LinkCollectionView({
+						el : $(getNewLinkDiv()),
+						collection : new Directory()
+					});
+
+					subLinks.collection.add(linksNew.toJSON(), {
+						silent : true
+					});
+
+					subLinks.setCollectionNumber(nextCollectionNumber);
+					subLinks.doInitialize();
+					subLinks.render();
+
+					allCollections.push(subLinks);
+				}
+
+				$(e.target).html("Less");
+				
+				linkRouter.navigate("#nav" + navNum++, { trigger: true });
+
+			} else if (btnTxt === "Less") {
+
+				var startCount = this.model.get("numId") + 1;
+				var lastCount = allCollections.length;
+
+				var testStr = "";
+				
+				for (var i = startCount; i < lastCount; i++) {
+					
+					var subColl = allCollections[i];
+					
+					for (var j = subColl.collection.models.length - 1; j > -1; j--) {
+						
+						var modelName = subColl.collection.models[j].get("name");
+						
+						if (modelName === nm) {
+							subColl.collection.models[j].destroy();
+						}
+						
+						subColl.render();
+					}
+				}
+				
+				$(e.target).html("More");
+				
+				linkRouter.navigate("#nav" + navNum++, { trigger: true });
+			}
+			
+			//var triggerName = "more";
+			//this.trigger("change:triggerName");
+			//window.navigate("more");
+			//this.navigate("#more", {trigger: true});
+			//window.location.href("more");
+			
+			//linkRouter.navigate($(this).attr("href"), { trigger: true });
+			//linkRouter.navigate("#more", { trigger: true });
+		}
+	});
+
+	var LinkCollectionView = Backbone.View.extend({
+		collectionNumber : 0,
+
+		setCollectionNumber : function(newCollectionNumber) {
+			this.collectionNumber = newCollectionNumber;
+		},
+
+		getCollectionNumber : function() {
+			return this.collectionNumber;
+		},
+
+		initialize : function() {
+		},
+
+		doInitialize : function() {
+			_.each(this.collection.models, function(item) {
+				item.set("numId", this.getCollectionNumber());
+			}, this);
+
+			this.render();
+		},
+
+		render : function() {
+			this.$el.html("");
+			var that = this;
+			_.each(this.collection.models, function(item) {
+				that.renderLink(item);
+			}, this);
+		},
+
+		renderLink : function(item) {
+			var linkView = new LinkView({
+				model : item
+			});
+			this.$el.append(linkView.render().el);
+		}
+	});
+
+	function getNewLinkDiv() {
+		var linkDiv = document.createElement("DIV");
+		linkDiv.className = "linkCollection";
+		$("body").append(linkDiv);
+
+		return linkDiv;
+	}
+
+	var mainLinks = new LinkCollectionView({
+		el : $(getNewLinkDiv()),
+		collection : new Directory(lc.pageLinks),
+	});
+	mainLinks.setCollectionNumber(0);
+	mainLinks.doInitialize();
+
+	var allCollections = [mainLinks];
+	
+	
+	var navNum = 0;
+	
+	var LinkRouter = Backbone.Router.extend({
+		routes: {
+			"*nav": "showMoreOrLess",
+			"": "index"
+		},
+		
+		showMoreOrLess: function(more) {
+			//$("#header2").html("show");
+			//document.getElementById("header2").innerHTML = "hejhoj";
+			//this.navigate("#");
+			console.log(more);
+		},
+		
+		index: function() {
+			console.log("index");
+		}
+	});
+	
+	var linkRouter = new LinkRouter();
+	
+	//Backbone.history.start({ pushState: true });
+	Backbone.history.start();
+});
+
+
+
+
+
+
+
+
+
+
+
---
+++ LinkProj/lib/app/linkCollection.js
@@ -0,0 +1,117 @@
+define({
+	pageLinks : [{
+		"name" : "jQuery",
+		"address" : "http://jquery.com/",
+		"type" : "",
+		"description" : "",
+		"links" : [{
+			"name" : "",
+			"address" : "http://jqueryui.com/",
+			"type" : "library",
+			"description" : "jQuery UI",
+			"links" : [{
+				"name" : "test",
+				"address" : "#",
+				"type" : "",
+				"description" : "",
+				"links" : []
+			}]
+		}, {
+			"name" : "",
+			"address" : "http://api.jquery.com/",
+			"type" : "api",
+			"description" : "jQuery API",
+			"links" : []
+		}]
+	}, {
+		"name" : "Underscore",
+		"address" : "http://underscorejs.org/",
+		"type" : "",
+		"description" : "",
+		"links" : [{
+			"name" : "",
+			"address" : "http://underscorejs.org/docs/underscore.html",
+			"type" : "api",
+			"description" : "Annotated source code",
+			"links" : [{
+				"name" : "test 1",
+				"address" : "#",
+				"type" : "",
+				"description" : "",
+				"links" : []
+			}, {
+				"name" : "test 2",
+				"address" : "#",
+				"type" : "",
+				"description" : "",
+				"links" : []
+			}]
+		}, {
+			"name" : "",
+			"address" : "https://github.com/jashkenas/underscore/",
+			"type" : "source",
+			"description" : "Underscore on GitHub",
+			"links" : []
+		}]
+	}, {
+		"name" : "Backbone",
+		"address" : "http://backbonejs.org/",
+		"type" : "",
+		"description" : "",
+		"links" : [{
+			"name" : "",
+			"address" : "http://net.tutsplus.com/sessions/build-a-contacts-manager-using-backbone-js/",
+			"type" : "tutorial",
+			"description" : "Tutorial. Somewhat faulty",
+			"links" : []
+		}, {
+			"name" : "",
+			"address" : "https://github.com/jashkenas/backbone/",
+			"type" : "source",
+			"description" : "Backbone on GitHub",
+			"links" : []
+		}, {
+			"name" : "",
+			"address" : "https://github.com/jashkenas/backbone/wiki/Tutorials%2C-blog-posts-and-example-sites",
+			"type" : "tutorial",
+			"description" : "Tutorials m.m.",
+			"links" : []
+		}, {
+			"name" : "",
+			"address" : "https://github.com/addyosmani/backbone-fundamentals/blob/gh-pages/index.md",
+			"type" : "tutorial",
+			"description" : "En hel bok om Backbone p� GitHub",
+			"links" : []
+		}]
+	}, {
+		"name" : "JSON",
+		"address" : "https://github.com/douglascrockford/JSON-js",
+		"type" : "",
+		"description" : "",
+		"links" : [{
+			"name" : "",
+			"address" : "http://jsonlint.com/",
+			"type" : "program",
+			"description" : "JSONLint. Check code",
+			"links" : []
+		}, {
+			"name" : "",
+			"address" : "http://www.w3schools.com/json/default.asp",
+			"type" : "tutorial",
+			"description" : "Tutorial on w3schools",
+			"links" : []
+		}, {
+			"name" : "",
+			"address" : "http://stackoverflow.com/questions/552135/difference-between-json-js-and-json2-js",
+			"type" : "QandA",
+			"description" : "Difference between json.js and json2.js",
+			"links" : []
+		}]
+	}/*, {
+	 "name" : "Testing Testing Testing Testing Testing",
+	 "address" : "https://github.com/douglascrockford/JSON-js",
+	 "type" : "",
+	 "description" : "",
+	 "links" : []
+	 }*/]
+});
